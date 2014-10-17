e35611540f7c813b8eb10938ee7b8d0bbec4096e
Gary Justin <garyjustin@DFRFR0252206MACGJ.local>
2014-10-16 20:54:24 -0400

    Added Project tiles

--- client/app/carousel/carousel.controller.js
+++ client/app/carousel/carousel.controller.js
@@ -5,13 +5,22 @@ function CarouselCtrl($scope, petitions) {
 	$scope.slides = [];
 
 	petitions.get().then(function(response) {
-		response.forEach(function(item){	
+		response.sort(function(a, b) {
+			return a.petition.sortOrder - b.petition.sortOrder;
+		});
+		response.forEach(function(item) {
+			var signaturesAmt = Math.round(item.petition.stats.signatureCount / item.petition.stats.targetSignatureCount * 100),
+				fundingAmt = Math.round(item.petition.stats.dollarsCollected / item.petition.stats.targetDollarsCollected * 100);
+
 			$scope.slides.push({
 				title: item.petition.name,
 				description: item.petition.description,
 				image: item.petition.image[0].fileName,
 				location: item.petition.location.city + ', ' + item.petition.location.state,
-				category: item.petition.category
+				category: item.petition.category,
+				signatures: signaturesAmt,
+				funding: fundingAmt,
+				signaturesMet: signaturesAmt < 100 ? false : true
 			});
 		});
 	});
